89058c99633e9931285719b78b5f320d5e05166a
ruye19 <167624441+ruye19@users.noreply.github.com>
2024-05-13 07:12:37 -0700

    MergeSort.java

--- labExam1/question1 and 5/src/question1/MergeSort.java
+++ labExam1/question1 and 5/src/question1/MergeSort.java
@@ -1,5 +1,65 @@
 package question1;
 
 public class MergeSort {
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int leftSize = mid - start + 1;
+        int rightSize = end - mid;
+
+        int[] leftArr = new int[leftSize];
+        int[] rightArr = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftArr[i] = arr[start + i];
+        }
+
+        for (int j = 0; j < rightSize; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = start;
+
+        while (i < leftSize && j < rightSize) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftSize) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightSize) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 3};
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
 
 }
